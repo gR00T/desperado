a3fd9d744bc7958e8058f60f9a5aed962267022b
root <joshua.b.gonzales@gmail.com>
2017-11-12 19:31:42 -0600

    Update via Image

---
+++ data_files/desperado15/ranSock.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2.7
+import socket
+import struct
+import random
+
+def ABCD():
+    #9879oa8c09809ac908809
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(('localhost',random.choice(range(15000,15999))))
+    s.listen(1)
+
+    #90098908c0fdada098c908
+    while True:
+        try:
+            conn, addr = s.accept()
+            buff = conn.recv(64)
+            if len(buff) > 0:
+                if buff.strip('\n') == "328bd8fa998209ac17ede5e5c69c4e98":
+                    print "Correct! fdc8af931b5f9378b02d1ce4cf62a8e6"
+                    conn.shutdown(socket.SHUT_RDWR)
+                    conn.close()
+                else:
+                    print "Wrong, you sent: " + buff
+                    conn.shutdown(socket.SHUT_RDWR)
+                    conn.close()
+        except socket.error, ex:
+            pass
